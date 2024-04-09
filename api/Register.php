03bcb962a6b489f47e8eec3480e675f94d0b8937
ValexEvans <57923977+ValexEvans@users.noreply.github.com>
2024-04-09 12:14:09 -0400

    updated register php

--- api/Register.php
+++ api/Register.php
@@ -1,65 +1,47 @@
 <?php
 
-$inData = getRequestInfo();
+	$inData = getRequestInfo();
+	
+	$FirstName = $inData["FirstName"];
+	$LastName = $inData["LastName"];
+	$Login = $inData["Login"];
+	$Password = $inData["Password"];
 
-$login = $inData["login"];
-$password = $inData["password"];
-$FirstName = $inData["FirstName"];
-$LastName = $inData["LastName"];
-$email = $inData["email"]; // Added email field
+	$conn = new mysqli("localhost", "PHPUSER", "Val21212@S1n2o3w4w", "DB01"); 	
+	if ($conn->connect_error) {
+		returnWithError($conn->connect_error);
+	} else {
+		$stmt = $conn->prepare("INSERT INTO User (FirstName, LastName, Login, Password) VALUES (?, ?, ?, ?)");
+		$stmt->bind_param("ssss", $FirstName, $LastName, $Login, $Password);
+		$stmt->execute();
 
-$conn = new mysqli("localhost", "PHPUSER", "Val21212@S1n2o3w4w", "DB01"); 	
-if ($conn->connect_error) {
-    returnWithError($conn->connect_error);
-} else {
-    // Check if the login or email already exists
-    $stmt = $conn->prepare("SELECT * FROM User WHERE Login = ? OR Email = ?");
-    $stmt->bind_param("ss", $login, $email);
-    $stmt->execute();
-    $result = $stmt->get_result();
+		if ($stmt->affected_rows > 0) {
+			returnWithInfo("User registered successfully.");
+		} else {
+			returnWithError("Failed to register user.");
+		}
 
-    if ($result->num_rows > 0) {
-        returnWithError("This login or email is already taken");
-        $stmt->close();
-    } else {
-        // Insert new user
-        $stmt = $conn->prepare("INSERT INTO User (Login, Password, FirstName, LastName, Email) VALUES (?, ?, ?, ?, ?)");
-        $stmt->bind_param("sssss", $login, $password, $FirstName, $LastName, $email);
-        $stmt->execute();
+		$stmt->close();
+		$conn->close();
+	}
+	
+	function getRequestInfo() {
+		return json_decode(file_get_contents('php://input'), true);
+	}
 
-        if ($stmt->affected_rows > 0) {
-            $insertedUserID = $stmt->insert_id;
-            returnWithInfo($FirstName, $LastName, $email, $insertedUserID);
-        } else {
-            returnWithError("Error in registering user");
-        }
-
-        $stmt->close();
-    }
-
-    $conn->close();
-}
-
-function getRequestInfo()
-{
-    return json_decode(file_get_contents('php://input'), true);
-}
-
-function sendResultInfoAsJson($obj)
-{
-    header('Content-type: application/json');
-    echo $obj;
-}
-
-function returnWithError($err)
-{
-    $retValue = '{"UserID":0, "FirstName":"", "LastName":"", "email":"", "error":"' . $err . '"}';
-    sendResultInfoAsJson($retValue);
-}
-
-function returnWithInfo($FirstName, $LastName, $email, $UserID)
-{
-    $retValue = '{"UserID":' . $UserID . ', "FirstName":"' . $FirstName . '", "LastName":"' . $LastName . '", "email":"' . $email . '", "error":""}';
-    sendResultInfoAsJson($retValue);
-}
+	function sendResultInfoAsJson($obj) {
+		header('Content-type: application/json');
+		echo $obj;
+	}
+	
+	function returnWithError($err) {
+		$retValue = '{"error":"' . $err . '"}';
+		sendResultInfoAsJson($retValue);
+	}
+	
+	function returnWithInfo($info) {
+		$retValue = '{"info":"' . $info . '"}';
+		sendResultInfoAsJson($retValue);
+	}
+	
 ?>
