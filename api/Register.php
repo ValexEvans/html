8f819bdb0ca54529a2a97330a5a52ac50082f155
ValexEvans <57923977+ValexEvans@users.noreply.github.com>
2024-04-09 14:01:29 -0400

    changed  register php

--- api/Register.php
+++ api/Register.php
@@ -1,47 +1,47 @@
 <?php
 
-	$inData = getRequestInfo();
-	
-	$FirstName = $inData["FirstName"];
-	$LastName = $inData["LastName"];
-	$Login = $inData["login"];
-	$Password = $inData["password"];
+    $inData = getRequestInfo();
+    
+    $FirstName = $inData["FirstName"];
+    $LastName = $inData["LastName"];
+    $Login = $inData["login"];
+    $Password = $inData["password"];
 
-	$conn = new mysqli("localhost", "PHPUSER", "Val21212@S1n2o3w4w", "DB01"); 	
-	if ($conn->connect_error) {
-		returnWithError($conn->connect_error);
-	} else {
-		$stmt = $conn->prepare("INSERT INTO User (FirstName, LastName, Login, Password) VALUES (?, ?, ?, ?)");
-		$stmt->bind_param("ssss", $FirstName, $LastName, $Login, $Password);
-		$stmt->execute();
+    $conn = new mysqli("localhost", "PHPUSER", "Val21212@S1n2o3w4w", "DB01");   
+    if ($conn->connect_error) {
+        returnWithError($conn->connect_error);
+    } else {
+        $stmt = $conn->prepare("INSERT INTO User (FirstName, LastName, Login, Password) VALUES (?, ?, ?, ?)");
+        $stmt->bind_param("ssss", $FirstName, $LastName, $Login, $Password);
+        $stmt->execute();
 
-		if ($stmt->affected_rows > 0) {
-			returnWithInfo("User registered successfully.");
-		} else {
-			returnWithError("Failed to register user.");
-		}
+        if ($stmt->affected_rows > 0) {
+            returnWithInfo("User registered successfully.");
+        } else {
+            returnWithError("Failed to register user.");
+        }
 
-		$stmt->close();
-		$conn->close();
-	}
-	
-	function getRequestInfo() {
-		return json_decode(file_get_contents('php://input'), true);
-	}
+        $stmt->close();
+        $conn->close();
+    }
+    
+    function getRequestInfo() {
+        return json_decode(file_get_contents('php://input'), true);
+    }
 
-	function sendResultInfoAsJson($obj) {
-		header('Content-type: application/json');
-		echo $obj;
-	}
-	
-	function returnWithError($err) {
+    function sendResultInfoAsJson($obj) {
+        header('Content-type: application/json');
+        echo $obj;
+    }
+    
+    function returnWithError($err) {
         $retValue = '{"UserID":0, "FirstName":"", "LastName":"", "error":"' . $err . '"}';
         sendResultInfoAsJson($retValue);
-	}
-	
-	function returnWithInfo($info) {
-		$retValue = '{"info":"' . $info . '"}';
-		sendResultInfoAsJson($retValue);
-	}
-	
+    }
+    
+    function returnWithInfo($info) {
+        $retValue = '{"info":"' . $info . '"}';
+        sendResultInfoAsJson($retValue);
+    }
+    
 ?>
