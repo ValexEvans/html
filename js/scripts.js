53da4a2855d05aeba876d06c967d047c9629411c
ValexEvans <57923977+ValexEvans@users.noreply.github.com>
2024-04-09 13:56:16 -0400

    added register js

--- js/scripts.js
+++ js/scripts.js
@@ -5,64 +5,59 @@ let UserID = 0;
 let FirstName = "";
 let LastName = "";
 
-function doLogin()
-{
+function doLogin() {
 	UserID = 0;
 	FirstName = "";
 	LastName = "";
-	
+
 	let login = document.getElementById("loginName").value;
 	let password = document.getElementById("loginPassword").value;
-//	var hash = md5( password );
-	
+	//	var hash = md5( password );
+
 	document.getElementById("loginResult").innerHTML = "";
 
-	let tmp = {login:login,password:password};
-//	var tmp = {login:login,password:hash};
-	let jsonPayload = JSON.stringify( tmp );
-	
+	let tmp = { login: login, password: password };
+	//	var tmp = {login:login,password:hash};
+	let jsonPayload = JSON.stringify(tmp);
+
 	let url = urlBase + '/Login.' + extension;
 
 	let xhr = new XMLHttpRequest();
 	xhr.open("POST", url, true);
 	xhr.setRequestHeader("Content-type", "application/json; charset=UTF-8");
-	try
-	{
-		xhr.onreadystatechange = function() 
-		{
-			if (this.readyState == 4 && this.status == 200) 
-			{
-				let jsonObject = JSON.parse( xhr.responseText );
+	try {
+		xhr.onreadystatechange = function () {
+			if (this.readyState == 4 && this.status == 200) {
+				let jsonObject = JSON.parse(xhr.responseText);
 				UserID = jsonObject.UserID;
-		
-				if( UserID < 1 )
-				{		
+
+				if (UserID < 1) {
 					document.getElementById("loginResult").innerHTML = "User/Password combination incorrect";
 					return;
 				}
-		
+
 				FirstName = jsonObject.FirstName;
 				LastName = jsonObject.LastName;
 
 				saveCookie();
-	
+
 				window.location.href = "landing.html";
 			}
 		};
 		xhr.send(jsonPayload);
 	}
-	catch(err)
-	{
+	catch (err) {
 		document.getElementById("loginResult").innerHTML = err.message;
 	}
 
 }
 
+
 function doRegister() {
-	UserID = 0;
-	FirstName = "";
-	LastName = "";
-	
+	let UserID = 0;
+	let FirstName = "";
+	let LastName = "";
+
 	let firstName = document.getElementById("registerFirstName").value;
 	let lastName = document.getElementById("registerLastName").value;
 	let login = document.getElementById("registerLogin").value;
@@ -86,7 +81,7 @@ function doRegister() {
 	xhr.open("POST", url, true);
 	xhr.setRequestHeader("Content-type", "application/json; charset=UTF-8");
 	try {
-		xhr.onreadystatechange = function() {
+		xhr.onreadystatechange = function () {
 			if (this.readyState == 4 && this.status == 200) {
 				let jsonObject = JSON.parse(xhr.responseText);
 				UserID = jsonObject.UserID;
@@ -103,54 +98,46 @@ function doRegister() {
 			}
 		};
 		xhr.send(jsonPayload);
-	} catch(err) {
+	} catch (err) {
 		document.getElementById("registerResult").innerHTML = err.message;
 	}
 }
 
-function saveCookie()
-{
+
+function saveCookie() {
 	let minutes = 20;
 	let date = new Date();
-	date.setTime(date.getTime()+(minutes*60*1000));	
+	date.setTime(date.getTime() + (minutes * 60 * 1000));
 	document.cookie = "FirstName=" + FirstName + ",LastName=" + LastName + ",UserID=" + UserID + ";expires=" + date.toGMTString();
 }
 
-function readCookie()
-{
+function readCookie() {
 	UserID = -1;
 	let data = document.cookie;
 	let splits = data.split(",");
-	for(var i = 0; i < splits.length; i++) 
-	{
+	for (var i = 0; i < splits.length; i++) {
 		let thisOne = splits[i].trim();
 		let tokens = thisOne.split("=");
-		if( tokens[0] == "FirstName" )
-		{
+		if (tokens[0] == "FirstName") {
 			FirstName = tokens[1];
 		}
-		else if( tokens[0] == "LastName" )
-		{
+		else if (tokens[0] == "LastName") {
 			LastName = tokens[1];
 		}
-		else if( tokens[0] == "UserID" )
-		{
-			UserID = parseInt( tokens[1].trim() );
+		else if (tokens[0] == "UserID") {
+			UserID = parseInt(tokens[1].trim());
 		}
 	}
-	
-	if( UserID < 0 )
-	{
+
+	if (UserID < 0) {
 		window.location.href = "index.html";
 	}
-	else
-	{
+	else {
 		document.getElementById("userName").innerHTML = "Logged in as " + FirstName + " " + LastName;
 	}
 }
 
-function doLogout()
-{
+function doLogout() {
 	UserID = 0;
 	FirstName = "";
 	LastName = "";
