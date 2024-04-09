71dacebdcd8b651e7a54db4af2c02c28d8616670
ValexEvans <57923977+ValexEvans@users.noreply.github.com>
2024-04-09 16:05:45 -0400

    added php

---
+++ api/UserRole.php
@@ -0,0 +1,46 @@
+<?php
+
+$conn = new mysqli("localhost", "PHPUSER", "Val21212@S1n2o3w4w", "DB01");
+
+if ($conn->connect_error) {
+    returnWithError($conn->connect_error);
+} else {
+    $requestData = json_decode(file_get_contents('php://input'), true);
+    $UserID = $requestData["UserID"];
+
+    $stmt = $conn->prepare("SELECT Role FROM User WHERE UserID = ?");
+    $stmt->bind_param("i", $UserID);
+    $stmt->execute();
+    $result = $stmt->get_result();
+
+    if ($result->num_rows > 0) {
+        $row = $result->fetch_assoc();
+        $role = $row["Role"];
+        returnWithInfo($role);
+    } else {
+        returnWithError("User not found.");
+    }
+
+    $stmt->close();
+    $conn->close();
+}
+
+function sendResultInfoAsJson($obj)
+{
+    header('Content-type: application/json');
+    echo json_encode($obj);
+}
+
+function returnWithError($err)
+{
+    $retValue = '{"error":"' . $err . '"}';
+    sendResultInfoAsJson($retValue);
+}
+
+function returnWithInfo($info)
+{
+    $retValue = '{"role":"' . $info . '"}';
+    sendResultInfoAsJson($retValue);
+}
+
+?>
