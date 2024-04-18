d7e2f4dd4212eaa0399e5fec4a6ccb6894cfae5e
ValexEvans <57923977+ValexEvans@users.noreply.github.com>
2024-04-18 01:33:24 -0400

    added University js

--- js/scripts.js
+++ js/scripts.js
@@ -459,6 +459,29 @@ function addRSO() {
 	}
 }
 
+function joinRSO(RSOID) {
+	var storedUserID = localStorage.getItem("userID"); // Retrieve user ID from local storage
+    let url = urlBase + '/JoinRSO.' + extension;
+    let xhr = new XMLHttpRequest();
+    
+    xhr.open("POST", url, true);
+    xhr.setRequestHeader("Content-Type", "application/json");
+
+    xhr.onreadystatechange = function () {
+        if (this.readyState == 4 && this.status == 200) {
+            let response = JSON.parse(xhr.responseText);
+            // Handle response as needed
+        }
+    };
+
+    let data = JSON.stringify({
+        UserID: storedUserID,
+        RSOID: RSOID
+    });
+
+    xhr.send(data);
+}
+
 
 function listRSO() {
     // let RsoName = document.getElementById("RsoName").value;
@@ -492,6 +515,7 @@ function listRSO() {
 
     xhr.send();
 }
+
 
 
 
