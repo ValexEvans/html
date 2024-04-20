7610f41d049335f358a01f9b464430a0c7f19259
ValexEvans <57923977+ValexEvans@users.noreply.github.com>
2024-04-19 21:50:46 -0400

    added joinRSO js

--- js/student-scripts.js
+++ js/student-scripts.js
@@ -1,43 +1,4 @@
-function joinRSO(RSOID) {
-	let storedUserID = String(localStorage.getItem("userID")); // Retrieve user ID from local storage
-	let rsoID = String(RSOID); // Convert RSOID to string
-	let request = "1"; // it is a request
 
-	let formData = {
-		UserID: storedUserID,
-		RsoID: rsoID,
-		Request: request
-	};
-
-
-
-	let jsonPayload = JSON.stringify(formData);
-	document.getElementById("youKnow").innerText = jsonPayload;
-	document.getElementById("createUniversityResult").innerHTML = "";
-
-
-	let xhr = new XMLHttpRequest();
-	let url = urlBase + '/RegisterRSO.' + extension;
-
-	xhr.open("POST", url, true);
-	xhr.setRequestHeader("Content-type", "application/json; charset=UTF-8");
-	try {
-		xhr.onreadystatechange = function () {
-			if (this.readyState == 4 && this.status == 200) {
-
-				document.getElementById("joinRSOResult").innerHTML = "Joined successfully";
-
-				// Optionally, you can redirect the user to the login page after successful registration
-				//window.location.reload();
-				//clearFormFields();
-			}
-		};
-		xhr.send(jsonPayload);
-	} catch (err) {
-		document.getElementById("joinRSOResult").innerHTML = err.message;
-	}
-
-}
 
 
 
@@ -68,33 +29,3 @@ function joinRSO(RSOID) {
 
 
 
-function listRsoSelect() {
-    let url = urlBase + '/ListRSO.' + extension;
-    let xhr = new XMLHttpRequest();
-
-    xhr.open("GET", url, true);
-
-    xhr.onreadystatechange = function () {
-        if (this.readyState == 4 && this.status == 200) {
-            let response = JSON.parse(xhr.responseText);
-            let selectOptions = '<option value="">Select RSO</option>'; // Default option
-
-
-            // Iterate through each RSO object in the response
-            response.forEach(function (rso) {
-               
-                // Add option for select element
-                selectOptions += '<option value="' + rso.RSOID +  '">' + rso.Name + '</option>';
-            });
-
-
-
-
-
-            // Update the select element with options
-            document.getElementById("rsoSelectionList").innerHTML = selectOptions;
-        }
-    };
-
-    xhr.send();
-}
