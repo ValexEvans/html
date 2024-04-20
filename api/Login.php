ee14f5d66b9dd0938bb1418ec2d61720fdbf6ffc
ValexEvans <57923977+ValexEvans@users.noreply.github.com>
2024-04-19 20:34:53 -0400

    added joinRSO js

--- api/Login.php
+++ api/Login.php
@@ -6,7 +6,6 @@ $UserID = 0;
 $FirstName = "";
 $LastName = "";
 $Role = "";
-$UniversityID = "";
 
 $conn = new mysqli("localhost", "PHPUSER", "Val21212@S1n2o3w4w", "DB01"); 	
 if ($conn->connect_error) {
@@ -18,33 +17,7 @@ if ($conn->connect_error) {
     $result = $stmt->get_result();
 
     if ($row = $result->fetch_assoc()) {
-        $UserID = $row['UserID'];
-        $FirstName = $row['FirstName'];
-        $LastName = $row['LastName'];
-        $Role = $row['Role'];
-
-        // Check if the user is an admin or a student
-        if ($Role == 'Admin') {
-            $query = "SELECT UniversityID FROM Admin WHERE UserID=?";
-        } elseif ($Role == 'Student') {
-            $query = "SELECT UniversityID FROM Student WHERE UserID=?";
-        } else {
-            // Handle the case if the user is a SuperAdmin or any other role that doesn't have a UniversityID
-            //returnWithError("Role does not require UniversityID");
-            //exit();
-        }
-        
-        if ($Role != 'SuperAdmin') {
-        // Retrieve UniversityID based on user's role
-        $stmt = $conn->prepare($query);
-        $stmt->bind_param("i", $UserID);
-        $stmt->execute();
-        $result = $stmt->get_result();
-
-        $UniversityID = $row['UniversityID'];
-        returnWithInfo($FirstName, $LastName, $UserID, $Role, $UniversityID);
-        }
-
+        returnWithInfo($row['FirstName'], $row['LastName'], $row['UserID'], $row['Role']);
     } else {
         returnWithError("No Records Found");
     }
@@ -66,13 +39,13 @@ function sendResultInfoAsJson($obj)
 
 function returnWithError($err)
 {
-    $retValue = '{"UserID":0,"FirstName":"","LastName":"","Role":"","UniversityID":0,"error":"' . $err . '"}';
+    $retValue = '{"UserID":0,"FirstName":"","LastName":"","Role":"","error":"' . $err . '"}';
     sendResultInfoAsJson($retValue);
 }
 
-function returnWithInfo($FirstName, $LastName, $UserID, $Role, $UniversityID)
+function returnWithInfo($FirstName, $LastName, $UserID, $Role)
 {
-    $retValue = '{"UserID":' . $UserID . ',"FirstName":"' . $FirstName . '","LastName":"' . $LastName . '","Role":"' . $Role . '","UniversityID":' . $UniversityID . ',"error":""}';
+    $retValue = '{"UserID":' . $UserID . ',"FirstName":"' . $FirstName . '","LastName":"' . $LastName . '","Role":"' . $Role . '","error":""}';
     sendResultInfoAsJson($retValue);
 }
 
